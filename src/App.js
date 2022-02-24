bcbf2aa857da3c3df9dffeca50d2e5062f33b364
jwhong <jwhong98@gmail.com>
2022-02-24 15:49:05 -0800

    completed about section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import About from "./components/About/About";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <About />
     </>
   );
 }
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,22 @@
 import React from "react";
+import Button from "../Button/Button";
 import classes from "./About.module.css";
+import charts from "../../images/illustration-intro.svg";
 
 const About = () => {
   return (
     <div className={classes.about}>
       <div className={classes.aboutContainer}>
-        <div className={classes.textWrap}></div>
+        <div className={classes.textWrap}>
+          <h1>Bring everyone together to build better products.</h1>
+          <p>
+            Manage makes it simple for software teams to plan day-to-day tasks
+            while keeping the larger team goals in view.
+          </p>
+          <Button />
+        </div>
         <div className={classes.imgWrap}>
-          <img src="" alt="charts" className={classes.img} />
+          <img src={charts} alt="charts" className={classes.img} />
         </div>
       </div>
     </div>
